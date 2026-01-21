4e1698fece948b797ebe585074197f646b73fdb1
MinjiSeo16 <mjlove6630@naver.com>
2026-01-22 01:31:30 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/minjiseo/stockcompare/StockcompareApplication.java
@@ -0,0 +1,13 @@
+package com.minjiseo.stockcompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockcompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockcompareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minjiseo/stockcompare/StockcompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjiseo.stockcompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockcompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
