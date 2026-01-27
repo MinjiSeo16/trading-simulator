cb743935d9a2ca2e06b9c6c9fdf84e6f143b1373
MinjiSeo16 <mjlove6630@naver.com>
2026-01-27 17:33:54 +0900

    feat: 로그인 비즈니스 로직 및 사용자 검증 추가

--- src/main/java/com/minjiseo/tradingsimulator/domain/user/repository/UserRepository.java
+++ src/main/java/com/minjiseo/tradingsimulator/domain/user/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.minjiseo.tradingsimulator.domain.user.repository;
 import com.minjiseo.tradingsimulator.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // 이메일 중복 체크용
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
