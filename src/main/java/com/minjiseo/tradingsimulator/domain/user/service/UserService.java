3f9eb3bf8d6da23ea9a78fd2f848baff57b1830b
MinjiSeo16 <mjlove6630@naver.com>
2026-02-02 18:47:52 +0900

    feat: 회원가입 시 Wallet 생성 및 초기 자금 지급 로직 추가

--- src/main/java/com/minjiseo/tradingsimulator/domain/user/service/UserService.java
+++ src/main/java/com/minjiseo/tradingsimulator/domain/user/service/UserService.java
@@ -4,6 +4,8 @@ import com.minjiseo.tradingsimulator.domain.user.dto.SignupRequest;
 import com.minjiseo.tradingsimulator.domain.user.dto.SignupResponse;
 import com.minjiseo.tradingsimulator.domain.user.entity.User;
 import com.minjiseo.tradingsimulator.domain.user.repository.UserRepository;
+import com.minjiseo.tradingsimulator.domain.wallet.entity.Wallet;
+import com.minjiseo.tradingsimulator.domain.wallet.repository.WalletRepository;
 import com.minjiseo.tradingsimulator.global.exception.CustomException;
 import com.minjiseo.tradingsimulator.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final WalletRepository walletRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
@@ -29,6 +32,10 @@ public class UserService {
         String encodedPassword = passwordEncoder.encode(request.password());
         User user = User.of(request.email(), encodedPassword);
         User savedUser = userRepository.save(user);
+
+        // Wallet 생성 및 초기 가상 자금 지급
+        Wallet wallet = Wallet.createForSignup(savedUser);
+        walletRepository.save(wallet);
 
         return SignupResponse.from(savedUser);
     }
